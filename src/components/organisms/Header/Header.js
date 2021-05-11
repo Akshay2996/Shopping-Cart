ff0179d2fb2935a3fdc69e95783ed3be48177221
Akshay Sharma <akshaysharmaay@gmail.com>
2021-05-11 14:42:56 +0530

    Added header cart & Fix sass naming convention

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Logo from '../../atoms/Logo/Logo';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import Cart from '../../molecules/Cart/Cart';
 
 export default function Header() {
     return (
         <header className="header">
             <div className="header__sub-container">
                 <Logo />
-                <nav className="header__nav__items">
-                    <Link to="/" className="header__nav__items__link">Home</Link>
-                    <Link to="/products" className="header__nav__items__link">Products</Link>
+                <nav className="header__sub-container__nav__items">
+                    <Link to="/" className="header__sub-container__nav__items__link">Home</Link>
+                    <Link to="/products" className="header__sub-container__nav__items__link">Products</Link>
                 </nav>
-                <nav className="header__nav__login">
-                    <Link to="/signin" className="header__nav__login__link">SignIn</Link>
-                    <Link to="/register" className="header__nav__login__link">Register</Link>
-                </nav>
+                <div className="header__sub-container__nav-wrapper">
+                    <nav className="header__sub-container__nav-wrapper__login">
+                        <Link to="/signin" className="header__sub-container__nav-wrapper__login__link">SignIn</Link>
+                        <Link to="/register" className="header__sub-container__nav-wrapper__login__link">Register</Link>
+                    </nav>
+                    <Cart />
+                </div>
             </div>
         </header>
     )
