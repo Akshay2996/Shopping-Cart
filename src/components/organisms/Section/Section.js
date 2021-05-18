a5e5faad044fe49830f86f7247d30b8c75101293
Akshay Sharma <akshaysharmaay@gmail.com>
2021-05-18 18:13:54 +0530

    Routing on Homepage & some improvements

--- src/components/organisms/Section/Section.js
+++ src/components/organisms/Section/Section.js
@@ -4,13 +4,13 @@ import Content from '../../molecules/Content/Content';
 import './Section.scss';
 
 
-export default function Section({url, heading, text, button, order}) {
+export default function Section({url, heading, text, button, order, handleProduct, id}) {
     return (
         <section className="container">
             {order % 2 == 0 ? (
                 <>
                     <div className="container__content">
-                        <Content heading={heading} text={text} button={button} />
+                        <Content heading={heading} text={text} button={button} handleProduct={handleProduct} id={id} />
                     </div>
                     <Image source={url} alt={`${heading} Image`} />
                 </>
@@ -20,7 +20,7 @@ export default function Section({url, heading, text, button, order}) {
                 <>
                     <Image source={url} alt={`${heading} Image`} />
                     <div className="container__content">
-                        <Content heading={heading} text={text} button={button} />
+                        <Content heading={heading} text={text} button={button} handleProduct={handleProduct} id={id} />
                     </div>
                     
                 </>
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import Section from '../../organisms/Section/Section';
 import './Home.scss';
-import Categories from '../../../../server/categories/index.get.json';
 
-export default function Home() {
-    Categories.sort((a, b) => {
-        return a.order - b.order;
-    });
-
-    const filteredCategory = Categories.filter(category => category.enabled);
-
+export default function Home({ filteredCategory, handleProduct }) {
     return (
         <main className="home-container">
             {filteredCategory.map((category) => 
-                <Section key={category.id} url={category.imageUrl} heading={category.name} text={category.description} button={category.key} order={category.order}
+                <Section key={category.id} id={category.id} url={category.imageUrl} heading={category.name} text={category.description} button={category.key} order={category.order} handleProduct={handleProduct}
             />)}
         </main>
     )
