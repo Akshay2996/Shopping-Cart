221fcda7063edf50aa61730a82155428b55895f1
Akshay Sharma <akshaysharmaay@gmail.com>
2021-05-26 21:31:40 +0530

    Created custom hook & Improved other files

--- src/components/molecules/Dropdown/Dropdown.js
+++ src/components/molecules/Dropdown/Dropdown.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
-import './Dropdown.scss';
-import Button from '../../atoms/Button/Button';
+import React, { useState } from "react";
+import "./Dropdown.scss";
+import Button from "../../atoms/Button/Button";
 
 export default function Dropdown({ filteredCategory, handleProduct }) {
-    const [isActive, setIsActive] = useState(true);
+  const [isActive, setIsActive] = useState(true);
 
-    const handleDropdown = () => {
-        setIsActive(!isActive);
-    }
+  const handleDropdown = () => {
+    setIsActive(!isActive);
+  };
 
-    return (
-        <aside className="dropdown">
-            <Button onClick={() => handleDropdown()} className="dropdown__button" button={`Dropdown ▼`} />
-            <ul>
-                {filteredCategory.map((category) => (
-                    <li onClick={() => handleProduct(category.id)} key={category.id} className={`${isActive ? "dropdown__content" : "dropdown__content show"}`}>{category.name}</li>
-                ))}
-            </ul>
-        </aside>
-    )
+  return (
+    <div className="dropdown">
+      <Button
+        onClick={() => handleDropdown()}
+        className="dropdown__button"
+        button={`All Products ▼`}
+      />
+      <ul className="dropdown__content">
+        {filteredCategory.map((category) => (
+          <li
+            onClick={() => {
+              handleDropdown();
+              handleProduct(category.id);
+            }}
+            key={category.id}
+            className={`${
+              isActive
+                ? "dropdown__content__list"
+                : "dropdown__content__list show"
+            }`}
+          >
+            {category.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { ProductContext } from '../../../contexts/ProductContext';
-import Dropdown from '../../molecules/Dropdown/Dropdown';
-import Sidebar from '../../molecules/Sidebar/Sidebar';
-import Card from '../../organisms/Card/Card';
-import './Products.scss';
+import React, { useContext } from "react";
+import { ProductContext } from "../../../contexts/ProductContext";
+import { useMediaQuery } from "../../../utils/useMediaQuery";
+import Dropdown from "../../molecules/Dropdown/Dropdown";
+import Sidebar from "../../molecules/Sidebar/Sidebar";
+import Card from "../../organisms/Card/Card";
+import "./Products.scss";
 
-export default function Products({ filteredCategory, filteredProduct, handleProduct}) {
-    // const { filteredProduct, handleProduct } = useContext(ProductContext);
+export default function Products() {
+  const { filteredCategory, filteredProduct, handleProduct } =
+    useContext(ProductContext);
 
-    return (
-        <main className="product-container">
-            <Sidebar filteredCategory={filteredCategory} handleProduct={handleProduct} />
-            <Dropdown filteredCategory={filteredCategory} handleProduct={handleProduct} />
-            <main className="product-container__card">
-                {filteredProduct && filteredProduct.map((product) => (
-                    <Card key={product.id} imageUrl={product.imageURL} name={product.name} price={product.price} stock={product.stock} text={product.description} category={product.category} />
-                 ) )}
-            </main>
-        </main>
-    )
+  const browserWidth = useMediaQuery("(max-width: 480px)");
+
+  return (
+    <main className="product-container">
+      {browserWidth ? (
+        <Dropdown
+          filteredCategory={filteredCategory}
+          handleProduct={handleProduct}
+        />
+      ) : (
+        <Sidebar
+          filteredCategory={filteredCategory}
+          handleProduct={handleProduct}
+        />
+      )}
+      <main className="product-container__card">
+        {filteredProduct.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            imageUrl={product.imageURL}
+            name={product.name}
+            price={product.price}
+            stock={product.stock}
+            text={product.description}
+          />
+        ))}
+      </main>
+    </main>
+  );
 }
