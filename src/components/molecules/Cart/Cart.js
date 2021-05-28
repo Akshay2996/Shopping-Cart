19f27c937e8e6db077363f5df7f4291fb72ec7ea
Akshay Sharma <akshaysharmaay@gmail.com>
2021-05-28 19:07:43 +0530

    Context API State Management

--- src/components/molecules/Cart/Cart.js
+++ src/components/molecules/Cart/Cart.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CartImage from '../../../../static/images/cart.svg';
-import Image from '../../atoms/Image/Image';
-import './Cart.scss';
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import CartImage from "../../../../static/images/cart.svg";
+import { GlobalContext } from "../../../contexts/GlobalContext";
+import { useMediaQuery } from "../../../utils/useMediaQuery";
+import Image from "../../atoms/Image/Image";
+import "./Cart.scss";
 
-export default function Cart() {
-    let count = 0;
-    const cartItem = count === 1 ? `${count} item` : `${count} items`
-    return (
-        <Link to="/cartpage" className="cart-wrapper">
-            <Image source={CartImage} className={"cart-wrapper__image"} alt={"Cart Image"} />
-            <p className="cart-wrapper__text">{cartItem}</p>
-        </Link>
-    )
+export default function Cart({ handleCart }) {
+  const {
+    cartItems: { count },
+  } = useContext(GlobalContext);
+
+  const countItem = count === 1 ? `${count} item` : `${count} items`;
+
+  const history = useHistory();
+  const browserWidth = useMediaQuery("(min-width: 769px)");
+
+  const handleBrowserWidth = () => {
+    browserWidth ? handleCart() : history.push("/cartpage");
+  };
+
+  return (
+    <>
+      <div
+        className="cart-wrapper"
+        onClick={() => {
+          handleBrowserWidth();
+        }}
+      >
+        <Image
+          source={CartImage}
+          className={"cart-wrapper__image"}
+          alt={"Cart Image"}
+        />
+        <p className="cart-wrapper__text">{countItem}</p>
+      </div>
+    </>
+  );
 }
